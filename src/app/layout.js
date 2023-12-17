f5995db0270850937e47af67f5210e9550320338
Jordan Maulana <jordanmaulana26@gmail.com>
2023-12-17 19:26:45 +0700

    Initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,21 @@
+import { DM_Sans as FontSans } from "next/font/google";
+import { Provider } from "@/components/provider";
+
+import "@/styles/globals.css";
+
+const fontSans = FontSans({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Digicommerce - devscale indonesia",
+  description: "Batch 3 Demo",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={fontSans.className}>
+        <Provider>{children}</Provider>
+      </body>
+    </html>
+  );
+}
