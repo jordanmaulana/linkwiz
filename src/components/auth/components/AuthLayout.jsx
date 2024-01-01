065dbdfaf3070bc9cc90f639f1129216257c8e81
adgojoe <jmaulana@adgorithmics.com>
2024-01-01 12:08:08 +0700

    chore: auth layout style

--- src/components/auth/components/AuthLayout.jsx
+++ src/components/auth/components/AuthLayout.jsx
@@ -1,9 +1,29 @@
+import Image from "next/image";
 import React from "react";
 
 export const AuthLayout = ({ children }) => {
   return (
-    <main className="h-screen grid grid-cols-1 lg:grid-cols-2 ">
-      <section></section>
+    <main className="h-screen grid grid-cols-1 lg:grid-cols-3 ">
+      <section className="flex flex-col items-start justify-center p-16 max-w-2xl mb-32 relative">
+        <Image
+          src="/spot.png"
+          width={355}
+          height={393}
+          className="absolute -z-10"
+        />
+        <div className="font-bold text-5xl">
+          Sign In to Start Boosting Your Sales
+        </div>
+        <div className="font-semibold mt-8">
+          Create 1 link for all your customer service agents
+        </div>
+      </section>
+      <Image
+        src="/rocket.png"
+        width={355}
+        height={393}
+        className="self-center  mt-24"
+      />
       <div className="flex justify-center items-center">
         <section>{children}</section>
       </div>{" "}
