591542e11817c11af13acf49dadf3a3e194c4e9e
adgojoe <jmaulana@adgorithmics.com>
2024-01-02 07:28:35 +0700

    chore: remove some code

--- src/components/dashboard/links/linksList.jsx
+++ src/components/dashboard/links/linksList.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { LinkCard } from "./linkCard";
+
 import {
   Table,
   TableHeader,
@@ -8,60 +8,21 @@ import {
   TableBody,
   TableRow,
   TableCell,
-  Tooltip,
-  Switch,
 } from "@nextui-org/react";
 
-import { EditIcon } from "./EditIcon";
-import { DeleteIcon } from "./DeleteIcon";
-
-import { columns } from "./data";
 import { LinkCell } from "./LinkCell";
 
 export const LinksList = ({ links }) => {
-  const renderCell = React.useCallback((link, columnKey) => {
-    const cellValue = link[columnKey];
-
-    switch (columnKey) {
-      case "name":
-        return <div>{link.name}</div>;
-      case "slug":
-        return <div>{link.slug}</div>;
-
-      case "agents":
-        return <div className="text-center">{link.agents.length}</div>;
-
-      case "isActive":
-        return (
-          <Switch
-            size="sm"
-            isSelected={link.isActive}
-            aria-label="Automatic updates"
-            // onValueChange={handleUpdate}
-          />
-        );
-      case "actions":
-        return (
-          <div className="relative flex items-center gap-2">
-            <Tooltip content="Edit user">
-              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                <EditIcon />
-              </span>
-            </Tooltip>
-            <Tooltip color="danger" content="Delete user">
-              <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                <DeleteIcon />
-              </span>
-            </Tooltip>
-          </div>
-        );
-      default:
-        return cellValue;
-    }
-  }, []);
+  const columns = [
+    { name: "NAME", uid: "name" },
+    { name: "SLUG", uid: "slug" },
+    { name: "STATUS", uid: "isActive" },
+    { name: "TOTAL AGENTS", uid: "agents" },
+    { name: "ACTIONS", uid: "actions" },
+  ];
 
   return (
-    <Table aria-label="Example table with custom cells">
+    <Table aria-label="Manage links">
       <TableHeader columns={columns}>
         {(column) => (
           <TableColumn
@@ -85,22 +46,5 @@ export const LinksList = ({ links }) => {
         )}
       </TableBody>
     </Table>
-  );
-
-  return (
-    <div className="grid grid-cols-3 gap-12 ">
-      {links?.map(({ id, name, slug, isActive, agents }) => {
-        return (
-          <LinkCard
-            key={id}
-            id={id}
-            name={name}
-            slug={slug}
-            isActive={isActive}
-            agents={agents}
-          />
-        );
-      })}
-    </div>
   );
 };
