42d23679eba88055234b6a2b6b771f1b5a6e32a9
safrony <tsoury@gmail.com>
2024-01-02 09:52:02 +0700

    masih error

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,8 @@ import "@/styles/globals.css";
 const fontSans = FontSans({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Digicommerce - devscale indonesia",
-  description: "Batch 3 Demo",
+  title: "LinkWiz",
+  description: "Keadilan Leads adalah hak setiap CS",
 };
 
 export default function RootLayout({ children }) {
---
+++ src/app/link/[url]/page.js
@@ -0,0 +1,55 @@
+import { NextResponse, NextRequest } from "next/server";
+import { prisma } from "@/utils/prisma";
+import { redirect } from "next/navigation";
+
+export default async function Page({ params }) {
+  //const id = params.id;
+
+  const findLink = await prisma.links.findUnique({
+    where: {
+      url: params.url,
+    },
+    select: {
+      is_active: true,
+      name: true,
+      id: true,
+    },
+  });
+
+  if (!findLink) {
+    return <div>Link id Not Found</div>;
+  }
+
+  console.log(`id: `, findLink.id);
+
+  const wa = await prisma.agents.findUnique({
+    where: {
+      id: "9f715a77-30a8-4139-8eeb-79820d0d7ffe",
+    },
+    select: {
+      isActive: true,
+      name: true,
+      id: true,
+      phone: true,
+    },
+  });
+
+  /*   const payLoad = {
+    url: findLink.url,
+    is_active: findLink.is_active,
+  }; */
+
+  //console.log(`WA :`, wa.phone);
+
+  //const linkurl = `https://wa.me/`, { wa.phone };
+
+  //console.log(linkurl);
+
+
+  if (findLink.is_active == true) {
+    redirect("https://wa.me/"${wa.phone});
+  }
+
+  //return <div>My Post: {findLink.is_active}</div>;
+  return <div>My Post: {params.url}</div>;
+}
