0acf88f4f22c764f443841ac955ee41557a4d7bc
adgojoe <jmaulana@adgorithmics.com>
2024-01-05 22:17:22 +0700

    feat: show chart

--- src/app/(dashboard)/dashboard/statistics/page.js
+++ src/app/(dashboard)/dashboard/statistics/page.js
@@ -19,5 +19,5 @@ export default async function Page() {
   const { data } = await getData();
   console.log(data);
 
-  return <Statistics />;
+  return <Statistics data={data} />;
 }
--- src/components/dashboard/statistics/Statistics.jsx
+++ src/components/dashboard/statistics/Statistics.jsx
@@ -1,5 +1,55 @@
-import React from "react";
+"use client";
 
-export const Statistics = () => {
-  return <div>Statistics</div>;
+import React from "react";
+import "chart.js/auto";
+
+import { Line } from "react-chartjs-2";
+
+const convertToHourlyBucket = (date) => {
+  const d = new Date(date);
+  const hour = d.getHours();
+
+  return `${hour}:00 - ${hour + 1}:00`;
+};
+
+const groupLeadsByHourlyBucket = (leads) => {
+  const hourlyBuckets = {};
+
+  leads.forEach((lead) => {
+    const bucket = convertToHourlyBucket(lead.date);
+
+    if (!hourlyBuckets[bucket]) {
+      hourlyBuckets[bucket] = 0;
+    }
+
+    hourlyBuckets[bucket]++;
+  });
+
+  return hourlyBuckets;
+};
+
+export const Statistics = ({ data }) => {
+  const leadsByHourlyBucket = groupLeadsByHourlyBucket(data);
+  console.log(leadsByHourlyBucket);
+
+  const hoursList = Object.keys(leadsByHourlyBucket);
+  const valuesList = Object.values(leadsByHourlyBucket);
+
+  const example = {
+    labels: hoursList,
+    datasets: [
+      {
+        fill: true,
+        label: "Total leads per hour",
+        data: valuesList,
+        borderWidth: 1,
+      },
+    ],
+  };
+  return (
+    <div>
+      <h1 className="font-bold text-xl my-3">Statistics</h1>
+      <Line data={example} width={400} height={100} />
+    </div>
+  );
 };
